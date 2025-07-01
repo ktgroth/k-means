b979d9841ce826eb4776c730795ed5593e1693e7
Kasen Groth <ktgoroth03@gmail.com>
2025-06-30 23:21:38 -0400

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {    
+        printf("");
+    } else {
+        if (!strcmp(argv[1], "--help"))
+            printf("Usage: .k-means [OPTION]... [FILE]\n"
+                   "Groups ");
+        else
+        {
+            for (size_t i = 0; i < argc; ++i)
+            {
+    
+            }
+        }
+
+    }
+
+    return 0;
+}
